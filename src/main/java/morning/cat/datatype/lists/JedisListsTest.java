d62eb738a7fc00f19cc13c4ec19944d57d159d3d
mengzhang6 <mei20160708??>
2019-04-10 15:45:49 +0800

    1. opi list

--- src/main/java/morning/cat/datatype/lists/JedisListsTest.java
+++ src/main/java/morning/cat/datatype/lists/JedisListsTest.java
@@ -2,6 +2,7 @@ package morning.cat.datatype.lists;
 
 import morning.cat.datatype.BaseTest;
 import org.junit.Test;
+import redis.clients.jedis.ListPosition;
 
 /**
  * @describe: {link https://redis.io/commands/#list}
@@ -22,9 +23,12 @@ public class JedisListsTest extends BaseTest {
 
     }
 
+    /**
+     * lset 重置特定位置的值
+     */
     @Test
     public void test2() {
-        // 重置
+        //
         jedis.lset(key + "key", 0, "data0000");
 
         jedis.lset(key + "key2", 0, "data0000");
@@ -34,30 +38,91 @@ public class JedisListsTest extends BaseTest {
     public void test3() {
 
         // 从首部弹出
-        String node = jedis.lpop(key + "key");
+        String node = jedis.lpop(key + "key2");
         System.out.println(node);
 
         // 从尾部弹出
-        String node2 = jedis.rpop(key + "key");
+        String node2 = jedis.rpop(key + "key2");
         System.out.println(node2);
 
     }
 
+    /**
+     * list 的长度
+     */
     @Test
     public void test4() {
-
+        Long length = jedis.llen(key + "key2");
+        System.out.println(length);
 
     }
 
+    /**
+     * lindex 获取特定位置的值
+     */
     @Test
     public void test5() {
+        String indexValue = jedis.lindex(key + "key2", 1);
+        System.out.println(indexValue);
+    }
 
+    /**
+     * linsert 第一个出现data2的位置之后插入333
+     */
+    @Test
+    public void test6() {
+        Long indexValue = jedis.linsert(key + "key2", ListPosition.AFTER, "data2", "333");
+        System.out.println(indexValue);
+    }
+
+    /**
+     * lrem  -- 删除 下标4 之后值为 data3 的节点
+     */
+    @Test
+    public void test7() {
+        Long indexValue = jedis.lrem(key + "key2", 4L, "data3");
+        System.out.println(indexValue);
+    }
+
+    /**
+     * ltrim 截断
+     */
+    @Test
+    public void test8() {
+        String value = jedis.ltrim(key + "key2", 1L, 3L);
+        System.out.println(value);
+    }
+
+    /**
+     * lpushx -- 从首部插入，与 lpush 的区别是，lpush插入的list不存在时会自动进行创建然后插入数据，lpushx 只有当list存在时进行插入操作
+     */
+    @Test
+    public void test9() {
+        Long value = jedis.lpushx(key + "key2", "aaa", "bbb", "ccc");
+        System.out.println(value);
     }
 
     @Test
-    public void test6() {
-
+    public void test10() {
+        Long value = jedis.rpushx(key + "key2", "qqq", "www", "eee");
+        System.out.println(value);
     }
 
+    /**
+     * {link http://redisdoc.com/list/rpoplpush.html}
+     * 将list1中的尾部数据弹出 插入到list2的首部
+     */
+    @Test
+    public void test11() {
+        String rvalue = jedis.rpoplpush(key + "key2", key + "key");
+        System.out.println(rvalue);
+    }
+
+    @Test
+    public void test12() {
+        jedis.blpop(key + "key2");
+        // ?
+
+    }
 
 }
--- src/main/java/morning/cat/datatype/sets/JedisSetsTest.java
+++ src/main/java/morning/cat/datatype/sets/JedisSetsTest.java
@@ -4,7 +4,9 @@ import morning.cat.datatype.BaseTest;
 import org.junit.Test;
 
 /**
- * @describe: {link https://redis.io/commands/#set}
+ * 官网文档：{link https://redis.io/commands/#set}
+ * 中文文档：{link http://redisdoc.com/set/index.html}
+ *
  * @author: morningcat.zhang
  * @date: 2019/4/9 5:04 PM
  */
@@ -13,6 +15,7 @@ public class JedisSetsTest extends BaseTest {
     @Test
     public void test1() {
         String key = "sets:test1:";
-
+        Long count = jedis.sadd(key + "key", "val1", "val2", "val3", "val4");
+        System.out.println(count);
     }
 }
